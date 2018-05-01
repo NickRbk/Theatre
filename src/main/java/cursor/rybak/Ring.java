0c3879273ecbac7ca9765a0559d688d7d686cf5f
Nick Rybak <nrybak94@gmail.com>
2018-05-01 10:29:05 +0300

    master: refactoring

--- src/main/java/cursor/rybak/Ring.java
+++ src/main/java/cursor/rybak/Ring.java
@@ -5,6 +5,9 @@ import cursor.rybak.util.TimeOut;
 import java.util.concurrent.CountDownLatch;
 
 public class Ring implements Runnable {
+    private final static int RING_COUNT = 3;
+    private final static int DELAY = 3000;
+
     private CountDownLatch ring;
 
     Ring(CountDownLatch ring) {
@@ -13,9 +16,6 @@ public class Ring implements Runnable {
 
     @Override
     public void run() {
-        final int RING_COUNT = 3;
-        final int DELAY = 3000;
-
         for (int i = 0; i < RING_COUNT; i++) {
             TimeOut.set(
                     String.format("Ring #%d", (RING_COUNT + 1) - (int) ring.getCount()),
